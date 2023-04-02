98fb7535a968b87093981824bf030bae3d9e0568
BadStudio <mail@badstudio.ru>
2023-04-02 23:30:35 +0700

    fix

--- yandex/api.php
+++ yandex/api.php
@@ -1,6 +1,6 @@
 <?
 # команда для получения IAM-токена: yc iam create-token
 // Укажите IAM-токен.
-define("TOKEN", "t1.9euelZrIzJKenJWeycvHypvNnJLNje3rnpWal8vGnouQz8_Mk5GOkM2OyJTl8_c_eFle-e9MICgM_d3z938mV17570wgKAz9.G0diLt6m9msRHzI95Twmsxfho5lViN3K_GS6NYV0O9_lnxrlL0UYCWcZB02QUpvXK0m-O2srG_pDbaLVlVCdCw");
+define("TOKEN", "demo");
 // Укажите идентификатор каталога.
-define("FOLDER", "b1ger9k7jbllreo3pb5m");
+define("FOLDER", "demo");
